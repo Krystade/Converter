d39467a33902c1c9031c4550d1eb8aa45d1db00b
Krys <jackpschmid@gmail.com>
2016-08-21 22:51:51 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -15,113 +15,130 @@ public class ConvertToDecimalToBinaryToHexa {
         System.out.print("Enter what you want to convert to,(decimal,binary,hexa) : ");
         String to = input.next();
         System.out.print("Enter the number you want to convert: ");
-        int fromNum = input.nextInt();
+        long fromNum = input.nextLong();
             if ("decimal".equals(from)){
                 if ("binary".equals(to)){
-                    System.out.println("from " + from + " to " + to);
-                }else if ("hexa".equals(to)){
-                    System.out.println("from " + from + " to " + to);
-                }
+                    String binary = "";
+                    if (fromNum > 0){
+                        while (fromNum > 0){
+                            long remainder = fromNum % 2;
+                            binary += remainder;
+                            fromNum = (fromNum / 2);
+                        }
+                    }else if (fromNum < 0){
+                        long posTen = Math.abs(fromNum);
+                        while (posTen > 0){
+                            long remainder = posTen % 2;
+                            binary += remainder;
+                            posTen = (posTen / 2);
+                            }
+                            String negative = "-";
+                            binary = negative + binary;
+                        }
+                        System.out.println(binary);
+                        
+                        }else if ("hexa".equals(to)){
+                            String hexa = "";
+                            if (fromNum > 0){
+                                while (fromNum > 0){
+                                    int remainder = (int) (fromNum % 16);
+                                    if (remainder < 10){
+                                        hexa += remainder;
+                                        fromNum = (fromNum / 16);
+                                    }else if (remainder > 10){
+                                        char remainderChar = 'Z';
+                                        switch (remainder){
+                                            case 10:
+                                                remainderChar = 'A';
+                                                break;
+                                            case 11:
+                                                remainderChar = 'B';
+                                                break;
+                                            case 12:
+                                                remainderChar = 'C';
+                                                break;
+                                            case 13:
+                                                remainderChar = 'D';
+                                                break;
+                                            case 14:
+                                                remainderChar = 'E';
+                                                 break;
+                                            case 15:
+                                                remainder = 'F';
+                                                break;
+                                        }
+                                        hexa += remainderChar;
+                                        fromNum = (fromNum / 16);
+                                    }
+                                }
+                            }
+                                else if (fromNum < 0){
+                                    fromNum = Math.abs(fromNum);
+                                    while (fromNum > 0){
+                                    int remainder = (int) (fromNum % 16);
+                                        char remainderChar = 'Z';
+                                        switch (remainder){
+                                            case 10:
+                                                remainderChar = 'A';
+                                                break;
+                                            case 11:
+                                                remainderChar = 'B';
+                                                break;
+                                            case 12:
+                                                remainderChar = 'C';
+                                                break;
+                                            case 13:
+                                                remainderChar = 'D';
+                                                break;
+                                            case 14:
+                                                remainderChar = 'E';
+                                                 break;
+                                            case 15:
+                                                remainder = 'F';
+                                                break;
+                                        }
+                                        hexa += remainderChar;
+                                        fromNum = (fromNum / 16);
+                                        }   
+                                        String negative = "-";
+                                        hexa = negative + hexa;
+                                      }
+                                    System.out.println(hexa);
+                        }
             }
-      
-      
             if ("binary".equals(from)){
                 if ("decimal".equals(to)){
-                    System.out.println("from " + from + " to " + to);
+                    
+                    
+                    
+                    
+                    
                 }else if ("hexa".equals(to)){
-                    System.out.println("from " + from + " to " + to);
+                    
+                    
+                    
+                    
+                    
                 }
             }
       
       
             if ("hexa".equals(from)){
                 if("decimal".equals(to)){
-                 System.out.println("from " + from + " to " + to);
+
+                    
+                    
+                    
+                    
                 } else if ("binary".equals(to)){
-                    System.out.println("from " + from + " to " + to);
+                    
+                    
+                    
+                    
+                    
+                    
                 }
             }
             System.out.println("End");
         }
     }
-/* System.out.print("Enter what you want to convert from,(decimal,binary,hexa) : ");
-  String from = input.next();
-  System.out.print("Enter what you want to convert to,(decimal,binary,hexa) : ");
-  String to = input.next();
-  System.out.print("Enter the number you want to convert: ");
-  int fromNum = input.nextInt();
-  if (from == "decimal"){
-    if (to == "binary"){
-        String binary = "";
-  if (fromNum > 0){
-    while (fromNum > 0){
-      int remainder = fromNum % 2;
-        binary += remainder;
-        fromNum = (fromNum / 2);
-        }   
-    }
-    else if (fromNum < 0){
-        int posTen = Math.abs(fromNum);
-        while (posTen > 0){
-      int remainder = posTen % 2;
-        binary += remainder;
-        posTen = (posTen / 2);
-        }   
-        String negative = "-";
-        binary = negative + binary;
-      }
-    System.out.println(binary);
-    }
-    else if (to == "hexa"){
-              String hexa = "";
-              if (fromNum > 0){
-                while (fromNum > 0){
-                  int remainder = fromNum % 16;
-                  if (remainder < 10){
-                  hexa += remainder;
-                  fromNum = (fromNum / 16);
-                  }else if (remainder > 10){
-                      char remainderChar = 'Z';
-                        switch (remainder){
-                            case 10:
-                                remainderChar = 'A';
-                                break;
-                            case 11:
-                                remainderChar = 'B';
-                                break;
-                            case 12:
-                                remainderChar = 'C';
-                                break;
-                            case 13:
-                                remainderChar = 'D';
-                                break;
-                            case 14:
-                                remainderChar = 'E';
-                                 break;
-                            case 15:
-                                remainder = 'F';
-                                break;
-                    }
-                        hexa += remainderChar;
-                        System.out.println(remainderChar);
-                        fromNum = (fromNum / 16);
-                        System.out.println(fromNum);
-                        
-                  }
-        }
-                
-    System.out.println(hexa);
-    }
-    else if (fromNum < 0){
-        int posTen = Math.abs(fromNum);
-        while (posTen > 0){
-      int remainder = posTen % 16;
-        hexa += remainder;
-        posTen = (posTen / 16);
-        }   
-        String negative = "-";
-        hexa = negative + hexa;
-      }
-    System.out.println(hexa);
-    }
-  }*/
